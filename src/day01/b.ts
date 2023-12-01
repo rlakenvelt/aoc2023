63ade7e72362f6a74ae573d741982931f3a7f019
Roel Lakenvelt <roel@lakenvelt.nl>
2023-12-01 17:22:33 +0100

    Some refactoring day 1

--- src/day01/b.ts
+++ src/day01/b.ts
@@ -9,33 +9,25 @@ const lines = input.getInput();
 logger.start();
 
 let answer = lines.map(wordsToDigits)
-                  .reduce((total, line) => {
+                  .reduce(accumulateNumbers, 0)
+
+logger.end(answer);
+
+function accumulateNumbers(total: number, line: string) {
     const digits: number[] = line.split('')
                                  .filter(digit => parseInt(digit))
                                  .map(digit => parseInt(digit));   
     total += digits[0] * 10 + digits[digits.length - 1];
-    return total;
-}, 0)
-
-logger.end(answer);
+    return total;    
+}
 
 function wordsToDigits(line: string) {
-    // quick&dirty...
-    line = line.replace(/oneight/g, 'oneeight');
-    line = line.replace(/twone/g, 'twoone');
-    line = line.replace(/threeight/g, 'threeeight');
-    line = line.replace(/fiveight/g, 'fiveeight');
-    line = line.replace(/sevenine/g, 'sevennine');
-    line = line.replace(/eightwo/g, 'eighttwo');
-    line = line.replace(/eighthree/g, 'eightthree');
-    line = line.replace(/nineight/g, 'nineeight');
-
     const words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     line = words.reduce((result, word, index) => {
-        return result.replace(new RegExp(`${word}`, 'g'), (index + 1).toString());
+        return result.replace(new RegExp(`${word}`, 'g'), word + (index + 1).toString() + word);
     }, line)
-
    return line;
-
 }
+
+
 
