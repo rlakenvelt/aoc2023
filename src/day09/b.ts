8d5fbc4dc72cce08de463f7216f2d43193c4ebc9
Roel Lakenvelt <roel@lakenvelt.nl>
2023-12-09 10:07:51 +0100

    Day 9

---
+++ src/day09/b.ts
@@ -0,0 +1,39 @@
+import InputHelper from '../utils/input';
+import Logger from '../utils/logger';
+import Common from '../utils/common';
+
+const puzzle = 'Day 09B: Mirage Maintenance'
+const input = new InputHelper();
+const logger = new Logger(puzzle);
+
+const inputValues = input.getInput().map(line=>line.split(' ').map(x=>parseInt(x)));
+
+logger.start();
+
+let answer = inputValues.map(nextValue).reduce(Common.total);
+logger.end(answer);
+
+function nextValue(values: number[]) {
+    let list = [...values]
+    const differencesList: number[][] = [values];
+    do {
+        const newlist = differences(list)
+        differencesList.unshift(newlist)
+        if (newlist.every(x=>x===0)) break
+        list=newlist
+    } while (true)
+    let nextValue = 0;
+    for (let i = 1; i < differencesList.length; i++) {
+        nextValue = differencesList[i][0] - nextValue;
+    }
+    return nextValue;
+}
+
+function differences(values: number[]) {
+    const list: number[] = [];
+    for (let i = 1; i < values.length; i++) {
+        list.push(values[i] - values[i - 1])
+    }
+    return list
+}
+
