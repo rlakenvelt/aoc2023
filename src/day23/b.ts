3000ea0b814ff843b06e45e54b6bb1888725861e
Roel Lakenvelt <roel@lakenvelt.nl>
2023-12-27 17:59:33 +0100

    Next unsuccesfull attempt day 23 part 2

--- src/day23/b.ts
+++ src/day23/b.ts
@@ -3,6 +3,7 @@ import Logger from '../utils/logger';
 import { Direction } from '../utils/direction'
 import { Point } from '../utils/point'
 import { Grid } from '../utils/grid'
+import { Graph } from '../utils/graph'
 
 interface Road {
     from: Point,
@@ -18,6 +19,7 @@ const inputValues = input.getInput().map(l=>l.split(''));
 const map: Grid<string> = new Grid(inputValues[0].length, inputValues.length)
 const crossroads: Set<number> = new Set()
 const roads: Road[] = []
+
 
 class Hike {
     path = new Set<number>()
@@ -60,53 +62,15 @@ const finish: Point = {x: map.width-2, y: map.height-1}
 logger.start();
 
 getCrossRoads()
-// console.log(crossroads)
 getRoads()
-// console.log(roads)
 
-// console.log(crossroads)
+const graph = new Graph<number>(true, true)
+roads.forEach(road=> {
+    graph.addEdge(key(road.from.x, road.from.y), key(road.to.x, road.to.y), road.length)
+})
+let result = graph.dijkstra(key(start.x, start.y), key(finish.x, finish.y))
 
-const result = bfs(roads, start, finish)
-// console.log(result)
-let answer = result.reduce((max, hike) => {
-  return Math.max(max, hike.length)
-}, 0);
-logger.end(answer);
-
-function bfs(roads: Road[], start: Point, finish: Point) {
-    const firstHike = new Hike()
-    firstHike.addToPath(roads[0])
-
-    const hikes: Hike[] = [firstHike];
-    const queue: Hike[] = [hikes[0]];
-    let loop=0
-    while (queue.length) {
-        loop++
-        // if (loop>2) break
-      const hike = queue.shift();
-      if (!hike) break
-      const current = hike.clone()
-    //   console.log(current)
-      if (current.x !== finish.x || current.y !== finish.y) {
-        // console.log('')
-        // console.log(loop, hike)
-        roads.filter(road=>road.from.x===hike.x && road.from.y===hike.y && !hike.visited(road.to.x, road.to.y))
-             .forEach((road, index)=> {
-                let tempHike = hike
-                if (index>0) {
-                  tempHike = current.clone()  
-                  hikes.push(tempHike)
-                }
-                tempHike.addToPath(road)   
-                queue.push(tempHike);
-
-             })
-      }
-
-    }
-    // console.log('HIKES', loop, hikes, finish.x, finish.y)
-    return hikes.filter(h=> h.x===finish.x&&h.y===finish.y);
-}
+logger.end(result?.costs);
 
 
 function key (x:number, y: number) {
@@ -138,9 +102,7 @@ function getCrossRoads() {
 function getRoads() {
     crossroads.forEach(c=> {
         const { x, y } = point(c)
-        // const x = c % 1000
-        // const y = (c - x) / 1000
-        // console.log(c, x, y)
+
         directionsForNode(x, y)
             .forEach(d=> {
                 const road: Road = {from: point(c), to: {x: 0, y: 0}, direction: d.symbol, length: 1}
